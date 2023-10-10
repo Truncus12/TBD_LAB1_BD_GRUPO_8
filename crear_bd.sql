09ea03c3027dfca5fcc8ccc8718a44158d9b30ff
Ignacio Riquelme <ignacio.riquelme.r@usach.cl>
2023-10-10 20:40:23 -0300

    cambio menor

--- crear_bd.sql
+++ crear_bd.sql
@@ -1,68 +1,68 @@
-CREATE OR REPLACE TABLE Voluntario (
+CREATE TABLE IF NOT EXISTS Voluntario (
     id SERIAL PRIMARY KEY,
     nombre VARCHAR(50) NOT NULL,
-    correo VARCHAR(257) NOT NULL
-    contrasena VARCHAR(257) NOT NULL,
+    correo VARCHAR(257) NOT NULL,
+    contrasena VARCHAR(257) NOT NULL
 );
 
-CREATE OR REPLACE TABLE Habilidad{
+CREATE TABLE IF NOT EXISTS Habilidad (
     id SERIAL PRIMARY KEY,
     nombre VARCHAR(50) NOT NULL
-};
+);
 
-CREATE OR REPLACE TABLE Institucion{
+CREATE TABLE IF NOT EXISTS Institucion (
     id SERIAL PRIMARY KEY,
     descripcion TEXT
-};
+);
 
-CREATE OR REPLACE TABLE Emergencia{
+CREATE TABLE IF NOT EXISTS Emergencia (
     id SERIAL PRIMARY KEY,
     nombre VARCHAR(50) NOT NULL,
     descripcion TEXT,
     fecha_inicio DATE,
     fecha_fin DATE,
     id_institucion BIGINT NOT NULL REFERENCES Institucion(id)
-};
+);
 
-CREATE OR REPLACE TABLE Tarea (
+CREATE TABLE IF NOT EXISTS Tarea (
     id SERIAL PRIMARY KEY,
     nombre VARCHAR(50) NOT NULL,
     descripcion TEXT,
-    cant_vol_req INT,
-    cant_vol_ins INT,
+    cant_vol_requeridos INT,
+    cant_vol_inscritos INT,
     fecha_inicio DATE,
     fecha_fin DATE,
     estado_actual CHAR,
     id_emergencia BIGINT NOT NULL REFERENCES Emergencia(id)
 );
 
-CREATE OR REPLACE TABLE Estado_Tarea{
+CREATE TABLE IF NOT EXISTS Estado_Tarea (
     id SERIAL PRIMARY KEY,
     descripcion TEXT,
     id_tarea BIGINT NOT NULL REFERENCES Tarea(id)
-};
+);
 
-CREATE OR REPLACE TABLE Ranking{
+CREATE TABLE IF NOT EXISTS Ranking (
     id SERIAL PRIMARY KEY,
     puntaje INT,
     flag_invitado BOOLEAN,
     flag_participa BOOLEAN,
     id_tarea BIGINT NOT NULL REFERENCES Tarea(id),
     id_voluntario BIGINT NOT NULL REFERENCES Voluntario(id)
-};
+);
 
-CREATE OR REPLACE TABLE Vol_Habilidad{
+CREATE TABLE IF NOT EXISTS Vol_Habilidad (
     id_habilidad BIGINT NOT NULL REFERENCES Habilidad(id),
     id_voluntario BIGINT NOT NULL REFERENCES Voluntario(id)
-};
+);
 
-CREATE OR REPLACE TABLE Eme_Habilidad{
+CREATE TABLE IF NOT EXISTS Eme_Habilidad (
     id_emergencia BIGINT NOT NULL REFERENCES Emergencia(id),
     id_habilidad BIGINT NOT NULL REFERENCES Habilidad(id)
-};
+);
 
-CREATE OR REPLACE TABLE Tarea_Habilidad{
+CREATE TABLE IF NOT EXISTS Tarea_Habilidad (
     id_emergencia BIGINT NOT NULL REFERENCES Emergencia(id),
     id_habilidad BIGINT NOT NULL REFERENCES Habilidad(id),
     id_tarea BIGINT NOT NULL REFERENCES Tarea(id)
-};
+);
