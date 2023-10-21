84205c4065c2374ee27db79c534da60381687ebf
Javier <javier.sanhueza.a@usach.cl>
2023-10-21 18:25:31 -0300

    mas elementos de llenado de tablas

--- poblar.sql
+++ poblar.sql
@@ -1,3 +1,43 @@
 -- Crea los estados de tarea
 INSERT INTO Estado_Tarea (id, descripcion)
 VALUES (1, 'Pendiente'), (2, 'Terminada'), (3, 'Cancelada');
+
+-- Crea institucion
+INSERT INTO Institucion (id, descripcion)
+VALUES (1,'Equipo de respuestas USACH'),
+		(2, 'Equipo de respuesta Metropolitano'),
+    	(3, 'Centro comunitario local'),
+    	(4, 'Cruz roja');
+
+-- Crea coordinador
+INSERT INTO Coordinador (id, nombre, correo, contrasena, institucion_asociada)
+VALUES (1,'Pedro', 'pedro@gmail.com', '123', 1),
+		(2, 'Maria', 'maria@gmail.com', '456', 2),
+      	(3, 'John', 'john@gmail.com', '789', 3),
+       	(4, 'Linda', 'linda@gmail.com', '123', 4);
+
+-- Crea las emergencia
+INSERT INTO Emergencia (id,nombre,descripcion,fecha_inicio,fecha_fin,coordinador_asociado)
+VALUES (1,'Mover arbol', 'Un arbol cayo y bloqueo una calle', '2023-10-23', '2023-10-24', 1),
+		(2, 'Inundacion', 'Fuerte lluvias inundaron un vecindario', '2023-10-25', '2023-10-26', 2),
+      	(3, 'Comuna sin electricidad', 'No hay electricidad en Estacion Central', '2023-10-27', '2023-10-28', 3),
+      	(4, 'Terremoto', 'Un terremoto boto varios semaforos', '2023-10-29', '2023-10-30', 4);
+
+-- Crea la tarea
+INSERT INTO Tarea (id, nombre, descripcion, cant_vol_requeridos, cant_vol_rinscritos, fecha_inicio, fecha_fin,
+					estado_actual,id_emergencia)
+VALUES (1,'conducir', 'conducir hasta donde cayo el arbol', 1, 0, '2023-10-23','2023-10-24','p',1),
+		(2, 'Rescate', 'Busqueda y rescate de victimas de inundacion', 5, 0, '2023-10-25', '2023-10-26', 'p', 2),
+       	(3, 'Crear refugio', 'Montar refugios temporales', 10, 0, '2023-10-27', '2023-10-28', 'p', 3),
+       	(4, 'Primeros auxilios', 'Primeros auxilios a personas afectadas por el terremoto', 8, 0, '2023-10-29', '2023-10-30', 'p', 4);
+
+
+-- Crea tarea x voluntario
+INSERT INTO Tarea_X_Voluntario (id_tarea, id_voluntario, estado)
+VALUES (1,1,1),
+		(2, 2, 1),
+       	(2, 3, 0),
+       	(3, 4, 1),
+       	(4, 2, 1),
+       	(4, 3, 0),
+       	(4, 4, 1);
